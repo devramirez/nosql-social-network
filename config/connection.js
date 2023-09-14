907292371a7e7b998d7dff5b89764c00ced8bc92
devramirez <aramirezdev1@gmail.com>
2023-09-14 00:42:46 -0400

    creating models

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const { connect, connection } = require('mongoose');
+const { connect, connection } = require('mongoose'); // establishing a connection to the MongoDB database
 
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
---
+++ models/Thought.js
@@ -0,0 +1,64 @@
+const { Schema, model, Types } = require('mongoose');
+
+const moment = require('moment'); // require moment.js
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+      // default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// Model for Thoughts
+const thoughtsSchema = new Schema(
+  {
+      thoughtText: {
+          type: String,
+          required: true,
+          minLength: 1,
+          maxLength: 280,
+      },
+      createdAt: {
+          // Moment.js time stamp
+          type: Date,
+          default: Date.now,
+          get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      },
+      username: {
+          type: String,
+          required: true,
+      },
+      reactions: [reactionSchema]
+  },
+  {
+      toJSON: {
+          virtuals: true,
+          getters: true
+      },
+      id: false
+  }
+);
+// module.exports = assignmentSchema;
