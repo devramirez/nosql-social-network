f121592791dffeb949c6101ae905aeda6aa9b9b8
devramirez <aramirezdev1@gmail.com>
2023-09-14 01:12:38 -0400

    created GET by ID

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -21,4 +21,24 @@ const thoughtsController = {
 
   }
 
-  
+// GET thought by ID
+getSingleThought({ params }, res) {
+  Thought.findOne({ _id: params.thoughtId })
+  .populate({
+      path: 'reactions',
+      select: '-__v'
+  })
+  .select('-__v')
+  .sort({ _id: -1 })
+  .then(dbThoughtData => {
+      if (!dbThoughtData) {
+          res.status(404).json({ message: 'No Thoughts found with this ID!' });
+          return;
+      }
+      res.json(dbThoughtData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+  });
+},
