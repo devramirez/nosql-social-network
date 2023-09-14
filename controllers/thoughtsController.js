b0a5bd958bb1588980d4560f9d1a0b946f52e0e8
devramirez <aramirezdev1@gmail.com>
2023-09-14 01:11:59 -0400

    creating thoughts controller

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,24 @@
+const { User, Thought } = require('../models');
+
+// Creates thoughts controller
+const thoughtsController = {
+
+    // GET thoughts
+    getThoughts(req, res) {
+        Thought.find({})
+        .populate({
+            path: 'reactions',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+  }
+
+  
